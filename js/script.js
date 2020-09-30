5bd9fd190b9adeee111eba1167619e77c725a6bc
Slava <slavaknyazev2003@gmail.com>
2020-09-30 21:10:10 +0600

    Добавил функцию скрытия контента, а так же функцию которая отображает 1 элемент контента на странице

---
+++ js/script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+window.addEventListener('DOMContentLoaded', () => {
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsContent = document.querySelectorAll('.tabcontent'),
+          blockParent = document.querySelector('.tabheader__items');
+    
+    function hideTabContent() {
+        tabsContent.forEach((item) => {
+            item.style.display = 'none';
+        })
+        tabs.forEach((item) => {
+            item.classList.remove('tabheader__item_active');
+        })
+    }
+
+    function showTabContent(i){
+        tabsContent[i].style.display = 'block';
+        tabsContent[i].classList.add('tabheader__item_active')
+    }
+    hideTabContent()
+    showTabContent(0)
+})
