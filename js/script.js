4bf810a6459a46e56250ddffe8265245eee5bc1b
Slava <slavaknyazev2003@gmail.com>
2020-10-01 16:34:54 +0600

    подключил классы скрытия и добавление контента, а так же плавную анимацию

--- js/script.js
+++ js/script.js
@@ -1,23 +1,42 @@
 'use strict';
 
 window.addEventListener('DOMContentLoaded', () => {
+    //получение элементов со страницы
     const tabs = document.querySelectorAll('.tabheader__item'),
           tabsContent = document.querySelectorAll('.tabcontent'),
-          blockParent = document.querySelector('.tabheader__items');
-    
+          tabsParent = document.querySelector('.tabheader__items');
+
+    //Функция которая скрывает контент и удаляет класс активности у элемента
     function hideTabContent() {
         tabsContent.forEach((item) => {
-            item.style.display = 'none';
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade')
         })
         tabs.forEach((item) => {
             item.classList.remove('tabheader__item_active');
         })
     }
 
-    function showTabContent(i){
-        tabsContent[i].style.display = 'block';
-        tabsContent[i].classList.add('tabheader__item_active')
+    //Функция которая отображает контент на странице по индексу элемента
+    function showTabContent(i = 0){
+        tabsContent[i].classList.remove('hide');
+        tabsContent[i].classList.add('show', 'fade');
+        tabs[i].classList.add('tabheader__item_active');
     }
+
+    //Использование делегирования событий на род. элементе, в котором лежат все табы
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+        if(target && target.classList.contains('tabheader__item')){
+            tabs.forEach((item, index) => {
+                if(target == item){
+                    hideTabContent()
+                    showTabContent(index)
+                }
+            })
+        }
+    })
+
     hideTabContent()
-    showTabContent(0)
+    showTabContent()
 })
